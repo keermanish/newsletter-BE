e3cc9a3a6787fb22c277e6f7ae6a20449160c59c
Keer <makeer@deloitte.com>
2019-03-25 15:51:32 +0530

    added support for some more domain fields

--- src/config/const.js
+++ src/config/const.js
@@ -1,5 +1,5 @@
 export const USER_FIELDS_TO_POPULATE = '_id name email phone designation';
 
-export const ALLOWED_USER_DOMAINS = ['UX', 'VD', 'FE', 'BE', 'PM', 'BA', 'QA'];
+export const ALLOWED_USER_DOMAINS = ['UX', 'VD', 'FE', 'BE', 'PM', 'BA', 'QA', 'All', 'FX', 'BxD', 'EM', 'iOS', 'Android'];
 
 export const AVOID_FIELDS_IN_RESPONSE = ['password', 'otp', 'otpExpire', '__v', 'iat', 'access'];
