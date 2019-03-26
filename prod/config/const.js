45a0d672290c65f42325a26d865b8d7a6f248bc7
Keer <makeer@deloitte.com>
2019-03-26 11:13:33 +0530

    added domain support in build package

--- prod/config/const.js
+++ prod/config/const.js
@@ -5,6 +5,6 @@ Object.defineProperty(exports, "__esModule", {
 });
 var USER_FIELDS_TO_POPULATE = exports.USER_FIELDS_TO_POPULATE = '_id name email phone designation';
 
-var ALLOWED_USER_DOMAINS = exports.ALLOWED_USER_DOMAINS = ['UX', 'VD', 'FE', 'BE', 'PM', 'BA', 'QA'];
+var ALLOWED_USER_DOMAINS = exports.ALLOWED_USER_DOMAINS = ['UX', 'VD', 'FE', 'BE', 'PM', 'BA', 'QA', 'All', 'FX', 'BxD', 'EM', 'iOS', 'Android'];
 
 var AVOID_FIELDS_IN_RESPONSE = exports.AVOID_FIELDS_IN_RESPONSE = ['password', 'otp', 'otpExpire', '__v', 'iat', 'access'];
